5ec846de6a19a74795e195fd2af2bed3c96e9e7f
Minh-Thang Pham <pmthang298@github.com>
2022-01-07 15:20:18 +0700

    Update new install.sh

--- install_zsh.sh
+++ install_zsh.sh
@@ -15,7 +15,7 @@ GITHUB_REPO="https://github.com/zsh-users"
 sudo git clone $GITHUB_REPO/zsh-syntax-highlighting /usr/local/share/zsh-syntax-highlighting
 sudo git clone $GITHUB_REPO/zsh-history-substring-search /usr/local/share/zsh-history-substring-search
 sudo git clone $GITHUB_REPO/zsh-autosuggestions /usr/local/share/zsh-autosuggestions
-git clone https://github.com/pmthang298/dotfiles $HOME/dotfiles
+# git clone https://github.com/pmthang298/dotfiles $HOME/dotfiles
 chsh -s $(which zsh)
-cp $HOME/dotfiles/.zshrc $HOME/
+cp .zshrc $HOME/
 zsh
